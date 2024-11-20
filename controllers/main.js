10876bbed4d998dcf9d64c4eaf4a0b9d16ca25da
Vu Nguyen <tanvumbs@yahoo.com>
2024-11-21 00:27:04 +0700

    search

--- controllers/main.js
+++ controllers/main.js
@@ -8,6 +8,7 @@ const btnUpdateSubmit = document.getElementById("btnUpdateSubmit");
 const headerTileModal = document.getElementById("header-title");
 
 const validation = new Validation();
+const arrNV = [];
 
 const renderTable = (data) => {
     let htmlContent = '';
@@ -30,11 +31,11 @@ const renderTable = (data) => {
                 </td>
                 <td>${qlnvServices.xepLoai(item.giolam)}</td>
                 <td>
-                    <button class="btn btn-warning" data-toggle="modal" 
+                    <button class="btn btn-warning w-100" data-toggle="modal" 
                     data-target="#myModal"
                     onClick="editNV('${item.id}')"
                     >Edit</button>
-                    <button class="btn btn-danger" onClick="deleteNV('${item.id}')">Delete</button>
+                    <button class="btn btn-danger mt-2 w-100" onClick="deleteNV('${item.id}')">Delete</button>
                 </td>
             </tr>
         `;
@@ -47,7 +48,15 @@ const getList = async () => {
     try {
         const res = await qlnvServices.getListAPI();
         if (res && res.data) {
-            renderTable(res.data);
+
+            res.data.forEach((item) => {
+                item.tongluong = qlnvServices.tongLuong(item.chucvu, item.luongCB);
+                item.xeploai = qlnvServices.xepLoai(item.giolam);
+                arrNV.push(item);
+            })
+            console.log(">>> arrNV: ", arrNV);
+
+            renderTable(arrNV);
         }
     } catch (err) {
         console.log('err: ', err);
@@ -175,3 +184,42 @@ window.deleteNV = async (id) => {
         console.log("Err");
     }
 }
+
+document.getElementById("btnSearch").onclick = () => {
+    const searchValue = document.getElementById("searchInput").value;
+    //console.log(searchValue.toLowerCase());
+
+    if (searchValue !== "") {
+        if (arrNV.length > 0) {
+            const resultArr = arrNV.filter((item) => {
+                return item.xeploai.toLowerCase() == searchValue.toLowerCase() || nonAccentVietnamese(item.xeploai.toLowerCase()) == nonAccentVietnamese(searchValue.toLowerCase());
+            })
+            renderTable(resultArr);
+        }
+    } else {
+        renderTable(arrNV);
+    }
+
+}
+
+// Hàm chuyển không dấu
+const nonAccentVietnamese = (str) => {
+    str = str.replace(/A|Á|À|Ã|Ạ|Â|Ấ|Ầ|Ẫ|Ậ|Ă|Ắ|Ằ|Ẵ|Ặ/g, "A");
+    str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+    str = str.replace(/E|É|È|Ẽ|Ẹ|Ê|Ế|Ề|Ễ|Ệ/, "E");
+    str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+    str = str.replace(/I|Í|Ì|Ĩ|Ị/g, "I");
+    str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+    str = str.replace(/O|Ó|Ò|Õ|Ọ|Ô|Ố|Ồ|Ỗ|Ộ|Ơ|Ớ|Ờ|Ỡ|Ợ/g, "O");
+    str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+    str = str.replace(/U|Ú|Ù|Ũ|Ụ|Ư|Ứ|Ừ|Ữ|Ự/g, "U");
+    str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+    str = str.replace(/Y|Ý|Ỳ|Ỹ|Ỵ/g, "Y");
+    str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+    str = str.replace(/Đ/g, "D");
+    str = str.replace(/đ/g, "d");
+    // Some system encode vietnamese combining accent as individual utf-8 characters
+    str = str.replace(/\u0300|\u0301|\u0303|\u0309|\u0323/g, ""); // Huyền sắc hỏi ngã nặng
+    str = str.replace(/\u02C6|\u0306|\u031B/g, ""); // Â, Ê, Ă, Ơ, Ư
+    return str;
+};
